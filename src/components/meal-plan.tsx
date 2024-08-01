d7e94b2d6b9012251d600898317703b78aaf7613
Stef <stefaniedavis@LaTashas-MacBook-Air.local>
2024-08-01 10:58:51 -0700

    comment out instacart widget

--- src/components/meal-plan.tsx
+++ src/components/meal-plan.tsx
@@ -75,13 +75,13 @@ export function MealPlan() {
   }
   useEffect(() => {
     // Load Instacart widget script
-    (function (d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) { return; }
-      js = d.createElement(s); js.id = id;
-      js.src = "https://widgets.instacart.com/widget-bundle-v2.js"; js.async = true;
-      js.dataset.source_origin = "affiliate_hub"; fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "standard-instacart-widget-v1");
+    // (function (d, s, id) {
+    //   var js, fjs = d.getElementsByTagName(s)[0];
+    //   if (d.getElementById(id)) { return; }
+    //   js = d.createElement(s); js.id = id;
+    //   js.src = "https://widgets.instacart.com/widget-bundle-v2.js"; js.async = true;
+    //   js.dataset.source_origin = "affiliate_hub"; fjs.parentNode.insertBefore(js, fjs);
+    // })(document, "script", "standard-instacart-widget-v1");
 
     // Add JSON-LD script for the grocery list schema
     const script = document.createElement('script');
